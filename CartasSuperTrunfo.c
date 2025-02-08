7262781d544f9f3f1fab9558b39fe45f488b4275
Moradillo91 <moradillodublin@gmail.com>
2025-02-08 12:16:37 +0000

    Ajuste no printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Lucas Moradillo Pregueiro
 
 int main() {
 
@@ -20,7 +16,7 @@ int main() {
 
     printf("Digite o Nome da cidade: \n");
     scanf("%s", &nome);
-    //
+
 
     printf("Digite o Código da Cidade: \n");
     scanf("%s", &codigo);
